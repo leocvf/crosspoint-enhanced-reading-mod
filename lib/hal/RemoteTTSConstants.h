b8499b863a9d5cd63f9fca9b5d3e05d1698eac9a
Leonardo Correzzola Villani Filho <leonardovillani1@hotmail.com>
2026-04-01 13:50:45 -0300

    Require BLE pairing for Remote TTS writes

--- lib/hal/RemoteTTSConstants.h
+++ lib/hal/RemoteTTSConstants.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstdint>
+
 // TODO: Replace with production UUIDs before shipping.
 static constexpr const char* X4_TTS_SERVICE_UUID = "0000fff0-0000-1000-8000-00805f9b34fb";
 static constexpr const char* X4_TTS_COMMAND_CHARACTERISTIC_UUID = "0000fff1-0000-1000-8000-00805f9b34fb";
+static constexpr uint32_t X4_TTS_PAIRING_PASSKEY = 123456;
